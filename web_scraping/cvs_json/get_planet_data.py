a5d3f5cb09ce12c3446388286fa53c3aa964c98d
TR-1000 <t.l.ross@outlook.com>
2020-03-30 03:29:16 -0500

    Completed cvs and serialization to json

--- web_scraping/cvs_json/get_planet_data.py
+++ web_scraping/cvs_json/get_planet_data.py
@@ -7,20 +7,20 @@ def get_planet_data():
     req = requests.get("http://localhost:8080/planets.html")
     soup = BeautifulSoup(req.text,"html.parser")
 
-    planet_rows = soup.find_all("tr", {"class": "planet"})
+    planet_rows =  soup.html.body.div.table.findAll("tr", {"class": "planet"})
     def to_dict(tr):
         tds = tr.findAll("td")
         planet_data = dict()
         planet_data['Name'] = tds[1].text.strip()
         planet_data['Mass'] = tds[2].text.strip()
         planet_data['Radius'] = tds[3].text.strip()
-        planet_data['Description'] = tds[4].text.strip()
+        planet_data['Description'] = tds[4].text.strip().replace("\r\n                    "," ")
         planet_data['MoreInfo'] = tds[5].findAll("a")[0]["href"].strip()
         return planet_data
     planets = [to_dict(tr) for tr in planet_rows]
     return planets
-# if __name__ == "__main__":
-#     print(get_planet_data())
+if __name__ == "__main__":
+    print(get_planet_data())
 
 
 planets = get_planet_data()
